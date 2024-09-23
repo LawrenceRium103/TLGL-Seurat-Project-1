7b02f4c35320a85b930d00d59a54cb56e42ab538
LawrenceRium103 <brx8wc@virginia.edu>
2024-09-22 23:34:00 -0400

    Update First_Project (Differential Gene and enriched Pathway in LGL vs Health Patient).R

--- First_Project (Differential Gene and enriched Pathway in LGL vs Health Patient).R
+++ First_Project (Differential Gene and enriched Pathway in LGL vs Health Patient).R
@@ -2,6 +2,7 @@
 library(Seurat)
 library(dplyr)
 library(ggplot2)
+library(ggrepel)
 library(clusterProfiler)
 
 pbmc <- readRDS('/standard/cphg-RLscratch/ar7jq/LGL_single_cell/lawrence/seurat.rds')
@@ -28,36 +29,32 @@ Idents(pbmc_Normal)
 table(Idents(pbmc_Normal))
 
 #Make a barplot of cell plot where 
-ggplot(pbmc, aes=(x=diagnosis)) + geom_bar(aes(fill=celltype))
+#ggplot(pbmc, aes=(x=diagnosis)) + geom_bar(aes(fill=celltype))
 
 #UMAP clustering of Normal cell type 
-DimPlot(pbmc_Normal)
+#DimPlot(pbmc_Normal)
 
 #Same Procedure for TLGL Patients
 Idents(object = pbmc_TLGL) <- "celltype"
 Idents(pbmc_TLGL)
 table(Idents(pbmc_TLGL))
-DimPlot(pbmc_TLGL)
+#DimPlot(pbmc_TLGL)
 
 #Create a new class that combine celltype and diagnosis status together
 pbmc$celltype.stim <-paste(pbmc$celltype, pbmc$diagnosis, sep="_")
 Idents(pbmc) <- "celltype.stim"
 
 #Sample differential Gene Analaysis (Treg)
-Treg.de <-FindMarkers(pbmc, ident.1='Treg_TLGL', ident.2="Treg_Normal", test.use="MAST", verbose=TRUE)
+Treg.de <-FindMarkers(pbmc, ident.1='Treg_TLGL', ident.2="Treg_Normal", latent.var=c("batch","donor","tissue"), test.use="MAST", verbose=TRUE)
 Treg.de$celltype <- rep('Treg', each=nrow(Treg.de))
-
-unique(pbmc$celltype)
-unique(pbmc_Normal$celltype)
-unique(pbmc_TLGL$celltype)
 
 
 #Create a meta dataset that combine RNA differential information of all celltype that present in both Normal and TLGL Patients.
 #Then plot of a volcano plot that colored by celltype. Label the signficant genes.
 
-Idents(object = pbmc) <- "celltype"
 
 Meta.de<- data.frame()
+latent_features<- 
 for(cell in unique(pbmc$celltype)){
   if(cell == 'TLGLL' | cell == 'NKLGLL'){
     next
@@ -68,21 +65,41 @@ for(cell in unique(pbmc$celltype)){
   
   
   
-  Temp.de<-FindMarkers(pbmc, ident.1=Normal_Iden, ident.2=TLGL_Iden, test.use='MAST', verbose=TRUE)
+  Temp.de<-FindMarkers(pbmc, ident.1=Normal_Iden, ident.2=TLGL_Iden, test.use='MAST', latent.var=c("batch","donor","tissue"), verbose=TRUE)
   Temp.de$celltype <- rep(cell, nrow(Temp.de))
   Meta.de<- rbind(Meta.de, Temp.de)
 }
+Meta.de$delabel <- NA
+Meta.de$delabel[which(Meta.de$p_val_adj<10^(-10))] <- row.names(Meta.de)[which(Meta.de$p_val_adj<10^(-10))]
 
-CD14_Mono.de <-FindMarkers(pbmc, ident.1 = 'CD14 Mono_Normal', ident.2="CD14 Mono_TLGL", test.use="MAST", verbose=TRUE)
-CD14_Mono_Treg.de <-merge()
+#Making Volcano Plot
+ggplot(data=Meta.de, aes(x=avg_log2FC, y=-log10(p_val_adj), col=celltype, label=delabel)) + 
+  geom_point() + 
+  geom_text_repel()
+  theme_minimal() +
+  geom_hline(yintercept=10, col="red")
 
 #ClusterProfiler
 
 #Prepare Background Annotation
 organism = "org.Hs.eg.db"
-BiocManager:: install (organism, character.only = TRUE)
 library(organism, character.only = TRUE)
 
+
 #import the metadata list used in the differential gene program
-original_gene_list <- 
+original_gene_list <- Meta.de$avg_log2FC
+names(original_gene_list)<-Meta.de$X
+gene_list<-na.omit(original_gene_list)
+gene_list = sort(gene_list, decreasing = TRUE)
+#Create GSE
+gse<-gseGO(geneList=gene_list, 
+      ont ="ALL", 
+      keyType = "GO", 
+      nPerm = 3, 
+      minGSSize = 3, 
+      maxGSSize = 400, 
+      pvalueCutoff = 10^(-10), 
+      verbose = TRUE, 
+      OrgDb = organism, 
+      pAdjustMethod = "none")
 
