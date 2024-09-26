27025a6963d8ac8b2abd71e82639c9fa1cd92f33
LawrenceRium103 <brx8wc@virginia.edu>
2024-09-26 18:14:21 -0400

    Update First_Project (Differential Gene and enriched Pathway in LGL vs Health Patient).R

--- First_Project (Differential Gene and enriched Pathway in LGL vs Health Patient).R
+++ First_Project (Differential Gene and enriched Pathway in LGL vs Health Patient).R
@@ -4,8 +4,11 @@ library(dplyr)
 library(ggplot2)
 library(ggrepel)
 library(clusterProfiler)
+library(doMC)
+
 
 pbmc <- readRDS('/standard/cphg-RLscratch/ar7jq/LGL_single_cell/lawrence/seurat.rds')
+DefaultAssay(object = pbmc)<-'RNA'
 
 colnames(pbmc)
 rownames(pbmc)
@@ -44,16 +47,28 @@ table(Idents(pbmc_TLGL))
 pbmc$celltype.stim <-paste(pbmc$celltype, pbmc$diagnosis, sep="_")
 Idents(pbmc) <- "celltype.stim"
 
+
 #Sample differential Gene Analaysis (Treg)
 Treg.de <-FindMarkers(pbmc, ident.1='Treg_TLGL', ident.2="Treg_Normal", latent.var="batch", test.use="MAST", verbose=TRUE)
 Treg.de$celltype <- rep('Treg', each=nrow(Treg.de))
+Treg.de$diffexpressed <- "NO"
+Treg.de$diffexpressed[Treg.de$avg_log2FC > 0.6 & Treg.de$p_val < 0.05] <- "UP"
+Treg.de$diffexpressed[Treg.de$avg_log2FC < -0.6 & Treg.de$p_val < 0.05] <- "DOWN"
 
+Treg.de$delabel <- NA
+Treg.de$delabel[which(Treg.de$p_val<0.05)] <- row.names(Treg.de)[which(Treg.de$p_val<0.05)]
 
+ggplot(data=Treg.de, aes(x=avg_log2FC, y=-log10(p_val), col=diffexpressed, label=delabel)) + 
+  geom_point() + 
+  geom_text_repel()+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"))+
+  geom_hline(yintercept=-log10(0.05), col="red")
 #Create a meta dataset that combine RNA differential information of all celltype that present in both Normal and TLGL Patients.
 #Then plot of a volcano plot that colored by celltype. Label the signficant genes.
 
+#Do it for every Celltype
 
-Meta.de<- data.frame()
 for(cell in unique(pbmc$celltype)){
   if(cell == 'TLGLL' | cell == 'NKLGLL'){
     next
@@ -64,18 +79,35 @@ for(cell in unique(pbmc$celltype)){
   
   
   
-  Temp.de<-FindMarkers(pbmc, ident.1=Normal_Iden, ident.2=TLGL_Iden, test.use='MAST', latent.var=c("batch","donor","tissue"), verbose=TRUE)
+  Temp.de<-FindMarkers(pbmc, ident.1=Normal_Iden, ident.2=TLGL_Iden, test.use='MAST', latent.var="batch", verbose=TRUE)
   Temp.de$celltype <- rep(cell, nrow(Temp.de))
-  Meta.de<- rbind(Meta.de, Temp.de)
+  Temp.de <-FindMarkers(pbmc, ident.1='Treg_TLGL', ident.2="Treg_Normal", latent.var="batch", test.use="MAST", verbose=TRUE)
+
+  Temp.de$diffexpressed <- "NO"
+  Temp.de$diffexpressed[Treg.de$avg_log2FC > 0.6 & Temp.de$p_val < 0.05] <- "UP"
+  Temp.de$diffexpressed[Treg.de$avg_log2FC < -0.6 & Temp.de$p_val < 0.05] <- "DOWN"
+  
+  Temp.de$delabel <- NA
+  Temp.de$delabel[which(Temp.de$p_val<0.05)] <- row.names(Temp.de)[which(Temp.de$p_val<0.05)]
+  
+  ggplot(data=Treg.de, aes(x=avg_log2FC, y=-log10(p_val), col=diffexpressed, label=delabel)) + 
+    geom_point() + 
+    ggtitle(str(cell) + "Volcano Plot")+
+    geom_text_repel()+
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+          panel.background = element_blank(), axis.line = element_line(colour = "black"))+
+    geom_hline(yintercept=-log10(0.05), col="red")
+  
+  Meta.de<-rbind(Meta.de, Temp.de)
+  
 }
-Meta.de$delabel <- NA
-Meta.de$delabel[which(Meta.de$p_val_adj<10^(-10))] <- row.names(Meta.de)[which(Meta.de$p_val_adj<10^(-10))]
 
 #Making Volcano Plot
 ggplot(data=Meta.de, aes(x=avg_log2FC, y=-log10(p_val_adj), col=celltype, label=delabel)) + 
   geom_point() + 
-  geom_text_repel()
-  theme_minimal() +
+  geom_text_repel()+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"))+
   geom_hline(yintercept=10, col="red")
 
 #ClusterProfiler
@@ -86,19 +118,26 @@ library(organism, character.only = TRUE)
 
 
 #import the metadata list used in the differential gene program
-original_gene_list <- Meta.de$avg_log2FC
-names(original_gene_list)<-Meta.de$X
+original_gene_list <- Treg.de$avg_log2FC
+names(original_gene_list)<-rownames(Treg.de)
 gene_list<-na.omit(original_gene_list)
 gene_list = sort(gene_list, decreasing = TRUE)
+ID<-mapIds(org.Hs.eg.db, keys = names(gene_list),
+       column = "ENTREZID", keytype = "ENSEMBL")
+names(gene_list)<-ID
 #Create GSE
 gse<-gseGO(geneList=gene_list, 
-      ont ="ALL", 
-      keyType = "GO", 
-      nPerm = 3, 
-      minGSSize = 3, 
-      maxGSSize = 400, 
-      pvalueCutoff = 10^(-10), 
+      ont ="ALL",
+      keyType = "ENTREZID",
+      minGSSize = 10, 
+      maxGSSize = 20, 
+      pvalueCutoff = 0.05, 
       verbose = TRUE, 
       OrgDb = organism, 
       pAdjustMethod = "none")
+
+
+require(DOSE)
+dotplot(gse, showCategory=10, split=".sign") + facet_grid(.~.sign)
+
 
