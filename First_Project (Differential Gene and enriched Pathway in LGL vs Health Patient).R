40ba34d6a19e1f854fb338d68898e15b14521c14
LawrenceRium103 <brx8wc@virginia.edu>
2024-09-27 11:58:42 -0400

    Update First_Project (Differential Gene and enriched Pathway in LGL vs Health Patient).R

--- First_Project (Differential Gene and enriched Pathway in LGL vs Health Patient).R
+++ First_Project (Differential Gene and enriched Pathway in LGL vs Health Patient).R
@@ -64,11 +64,40 @@ ggplot(data=Treg.de, aes(x=avg_log2FC, y=-log10(p_val), col=diffexpressed, label
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_blank(), axis.line = element_line(colour = "black"))+
   geom_hline(yintercept=-log10(0.05), col="red")
-#Create a meta dataset that combine RNA differential information of all celltype that present in both Normal and TLGL Patients.
-#Then plot of a volcano plot that colored by celltype. Label the signficant genes.
+
+#Sample GSE Analysis
+organism = "org.Hs.eg.db"
+library(organism, character.only = TRUE)
+
+original_gene_list <- Treg.de$avg_log2FC
+names(original_gene_list)<-rownames(Treg.de)
+gene_list<-na.omit(original_gene_list)
+gene_list = sort(gene_list, decreasing = TRUE)
+ID<-mapIds(org.Hs.eg.db, keys = names(gene_list),
+           column = "ENTREZID", keytype = "ENSEMBL")
+names(gene_list)<-ID
+#Create GSE
+gse<-gseGO(geneList=gene_list, 
+           ont ="ALL",
+           keyType = "ENTREZID",
+           minGSSize = 5, 
+           maxGSSize = 500, 
+           pvalueCutoff = 0.05, 
+           verbose = TRUE, 
+           OrgDb = organism, 
+           pAdjustMethod = "none")
+
+
+require(DOSE)
+dotplot(gse, showCategory=10, split=".sign") + facet_grid(.~.sign)
+
+
 
 #Do it for every Celltype
+library(BiocParallel)
+register(SerialParam())
 
+Meta.de<-NA
 for(cell in unique(pbmc$celltype)){
   if(cell == 'TLGLL' | cell == 'NKLGLL'){
     next
@@ -81,63 +110,58 @@ for(cell in unique(pbmc$celltype)){
   
   Temp.de<-FindMarkers(pbmc, ident.1=Normal_Iden, ident.2=TLGL_Iden, test.use='MAST', latent.var="batch", verbose=TRUE)
   Temp.de$celltype <- rep(cell, nrow(Temp.de))
-  Temp.de <-FindMarkers(pbmc, ident.1='Treg_TLGL', ident.2="Treg_Normal", latent.var="batch", test.use="MAST", verbose=TRUE)
 
+  
   Temp.de$diffexpressed <- "NO"
-  Temp.de$diffexpressed[Treg.de$avg_log2FC > 0.6 & Temp.de$p_val < 0.05] <- "UP"
-  Temp.de$diffexpressed[Treg.de$avg_log2FC < -0.6 & Temp.de$p_val < 0.05] <- "DOWN"
+  Temp.de$diffexpressed[Temp.de$avg_log2FC > 0.6 & Temp.de$p_val < 0.05] <- "UP"
+  Temp.de$diffexpressed[Temp.de$avg_log2FC < -0.6 & Temp.de$p_val < 0.05] <- "DOWN"
   
   Temp.de$delabel <- NA
   Temp.de$delabel[which(Temp.de$p_val<0.05)] <- row.names(Temp.de)[which(Temp.de$p_val<0.05)]
   
-  ggplot(data=Treg.de, aes(x=avg_log2FC, y=-log10(p_val), col=diffexpressed, label=delabel)) + 
+  title_volcano<- paste(cell,"Volcano_plot.png", sep="_")
+  ggplot(data=Temp.de, aes(x=avg_log2FC, y=-log10(p_val), col=diffexpressed, label=delabel)) + 
     geom_point() + 
-    ggtitle(str(cell) + "Volcano Plot")+
+    ggtitle(title)+
     geom_text_repel()+
     theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
           panel.background = element_blank(), axis.line = element_line(colour = "black"))+
     geom_hline(yintercept=-log10(0.05), col="red")
   
+  ggsave(title_dot,width=10, height=7, dpi=300,limitsize = FALSE)
+  
   Meta.de<-rbind(Meta.de, Temp.de)
+  
+  original_gene_list <- Temp.de$avg_log2FC
+  names(original_gene_list)<-rownames(Temp.de)
+  gene_list<-na.omit(original_gene_list)
+  gene_list = sort(gene_list, decreasing = TRUE)
+  ID<-mapIds(org.Hs.eg.db, keys = names(gene_list),
+             column = "ENTREZID", keytype = "SYMBOL")
+  names(gene_list)<-ID
+  #Create GSE
+  gse<-gseGO(geneList=gene_list, 
+             ont ="ALL",
+             keyType = "ENTREZID",
+             minGSSize = 5, 
+             maxGSSize = 500, 
+             pvalueCutoff = 0.05, 
+             verbose = TRUE, 
+             OrgDb = organism, 
+             pAdjustMethod = "none")
+
+  require(DOSE)
+  title_dot<-paste(cell,"_dotplot.png",sep="")
+  dotplot(gse, showCategory=10, split=".sign") + facet_grid(.~.sign)
+  ggsave(title_dot,width=7, height=10, dpi=300, limitsize = FALSE)
   
 }
 
 #Making Volcano Plot
-ggplot(data=Meta.de, aes(x=avg_log2FC, y=-log10(p_val_adj), col=celltype, label=delabel)) + 
-  geom_point() + 
-  geom_text_repel()+
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        panel.background = element_blank(), axis.line = element_line(colour = "black"))+
-  geom_hline(yintercept=10, col="red")
 
 #ClusterProfiler
 
 #Prepare Background Annotation
-organism = "org.Hs.eg.db"
-library(organism, character.only = TRUE)
 
-
-#import the metadata list used in the differential gene program
-original_gene_list <- Treg.de$avg_log2FC
-names(original_gene_list)<-rownames(Treg.de)
-gene_list<-na.omit(original_gene_list)
-gene_list = sort(gene_list, decreasing = TRUE)
-ID<-mapIds(org.Hs.eg.db, keys = names(gene_list),
-       column = "ENTREZID", keytype = "ENSEMBL")
-names(gene_list)<-ID
-#Create GSE
-gse<-gseGO(geneList=gene_list, 
-      ont ="ALL",
-      keyType = "ENTREZID",
-      minGSSize = 10, 
-      maxGSSize = 20, 
-      pvalueCutoff = 0.05, 
-      verbose = TRUE, 
-      OrgDb = organism, 
-      pAdjustMethod = "none")
-
-
-require(DOSE)
-dotplot(gse, showCategory=10, split=".sign") + facet_grid(.~.sign)
 
 
