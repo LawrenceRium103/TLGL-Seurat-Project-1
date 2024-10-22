c488feb8db5c4b86f083566dc85746ad2dfaddfb
LawrenceRium103 <brx8wc@virginia.edu>
2024-10-22 00:37:10 -0400

    Update First_Project (Differential Gene and enriched Pathway in LGL vs Health Patient).R

--- First_Project (Differential Gene and enriched Pathway in LGL vs Health Patient).R
+++ First_Project (Differential Gene and enriched Pathway in LGL vs Health Patient).R
@@ -48,54 +48,12 @@ pbmc$celltype.stim <-paste(pbmc$celltype, pbmc$diagnosis, sep="_")
 Idents(pbmc) <- "celltype.stim"
 
 
-#Sample differential Gene Analaysis (Treg)
-Treg.de <-FindMarkers(pbmc, ident.1='Treg_TLGL', ident.2="Treg_Normal", latent.var="batch", test.use="MAST", verbose=TRUE)
-Treg.de$celltype <- rep('Treg', each=nrow(Treg.de))
-Treg.de$diffexpressed <- "NO"
-Treg.de$diffexpressed[Treg.de$avg_log2FC > 0.6 & Treg.de$p_val < 0.05] <- "UP"
-Treg.de$diffexpressed[Treg.de$avg_log2FC < -0.6 & Treg.de$p_val < 0.05] <- "DOWN"
-
-Treg.de$delabel <- NA
-Treg.de$delabel[which(Treg.de$p_val<0.05)] <- row.names(Treg.de)[which(Treg.de$p_val<0.05)]
-
-ggplot(data=Treg.de, aes(x=avg_log2FC, y=-log10(p_val), col=diffexpressed, label=delabel)) + 
-  geom_point() + 
-  geom_text_repel()+
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        panel.background = element_blank(), axis.line = element_line(colour = "black"))+
-  geom_hline(yintercept=-log10(0.05), col="red")
-
-#Sample GSE Analysis
-organism = "org.Hs.eg.db"
-library(organism, character.only = TRUE)
-
-original_gene_list <- Treg.de$avg_log2FC
-names(original_gene_list)<-rownames(Treg.de)
-gene_list<-na.omit(original_gene_list)
-gene_list = sort(gene_list, decreasing = TRUE)
-ID<-mapIds(org.Hs.eg.db, keys = names(gene_list),
-           column = "ENTREZID", keytype = "ENSEMBL")
-names(gene_list)<-ID
-#Create GSE
-gse<-gseGO(geneList=gene_list, 
-           ont ="ALL",
-           keyType = "ENTREZID",
-           minGSSize = 5, 
-           maxGSSize = 500, 
-           pvalueCutoff = 0.05, 
-           verbose = TRUE, 
-           OrgDb = organism, 
-           pAdjustMethod = "none")
-
-
-require(DOSE)
-dotplot(gse, showCategory=10, split=".sign") + facet_grid(.~.sign)
-
-
-
 #Do it for every Celltype
 library(BiocParallel)
 register(SerialParam())
+
+organism = "org.Hs.eg.db"
+library(organism, character.only = TRUE)
 
 Meta.de<-NA
 for(cell in unique(pbmc$celltype)){
@@ -110,7 +68,7 @@ for(cell in unique(pbmc$celltype)){
   
   Temp.de<-FindMarkers(pbmc, ident.1=Normal_Iden, ident.2=TLGL_Iden, test.use='MAST', latent.var="batch", verbose=TRUE)
   Temp.de$celltype <- rep(cell, nrow(Temp.de))
-
+  
   
   Temp.de$diffexpressed <- "NO"
   Temp.de$diffexpressed[Temp.de$avg_log2FC > 0.6 & Temp.de$p_val < 0.05] <- "UP"
@@ -119,16 +77,16 @@ for(cell in unique(pbmc$celltype)){
   Temp.de$delabel <- NA
   Temp.de$delabel[which(Temp.de$p_val<0.05)] <- row.names(Temp.de)[which(Temp.de$p_val<0.05)]
   
-  title_volcano<- paste(cell,"Volcano_plot.png", sep="_")
-  ggplot(data=Temp.de, aes(x=avg_log2FC, y=-log10(p_val), col=diffexpressed, label=delabel)) + 
-    geom_point() + 
-    ggtitle(title)+
-    geom_text_repel()+
-    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-          panel.background = element_blank(), axis.line = element_line(colour = "black"))+
-    geom_hline(yintercept=-log10(0.05), col="red")
-  
-  ggsave(title_dot,width=10, height=7, dpi=300,limitsize = FALSE)
+
+  # title_volcano<- paste(cell,"Volcano_plot.png", sep="_")
+  # ggplot(data=Temp.de, aes(x=avg_log2FC, y=-log10(p_val), col=diffexpressed, label=delabel)) + 
+  #   geom_point() + 
+  #   geom_text_repel()+
+  #   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+  #         panel.background = element_blank(), axis.line = element_line(colour = "black"))+
+  #   geom_hline(yintercept=-log10(0.05), col="red")
+  # 
+  # ggsave(title_volcano,width=10, height=7, dpi=300,limitsize = FALSE)
   
   Meta.de<-rbind(Meta.de, Temp.de)
   
@@ -139,29 +97,71 @@ for(cell in unique(pbmc$celltype)){
   ID<-mapIds(org.Hs.eg.db, keys = names(gene_list),
              column = "ENTREZID", keytype = "SYMBOL")
   names(gene_list)<-ID
-  #Create GSE
-  gse<-gseGO(geneList=gene_list, 
-             ont ="ALL",
-             keyType = "ENTREZID",
-             minGSSize = 5, 
-             maxGSSize = 500, 
-             pvalueCutoff = 0.05, 
-             verbose = TRUE, 
-             OrgDb = organism, 
-             pAdjustMethod = "none")
-
-  require(DOSE)
-  title_dot<-paste(cell,"_dotplot.png",sep="")
-  dotplot(gse, showCategory=10, split=".sign") + facet_grid(.~.sign)
-  ggsave(title_dot,width=7, height=10, dpi=300, limitsize = FALSE)
+  
+  # #Create GSE
+  # gse<-gseGO(geneList=gene_list, 
+  #            ont ="ALL",
+  #            keyType = "ENTREZID",
+  #            minGSSize = 5, 
+  #            maxGSSize = 500, 
+  #            pvalueCutoff = 0.05, 
+  #            verbose = TRUE, 
+  #            OrgDb = organism, 
+  #            pAdjustMethod = "none")
+  # 
+  # require(DOSE)
+  # title_dot<-paste(cell,"_dotplot.png",sep="")
+  # dotplot(gse, showCategory=10, split=".sign") + facet_grid(.~.sign)
+  # ggsave(title_dot,width=7, height=10, dpi=300, limitsize = FALSE)
   
 }
+data<-read.csv("Meta.csv")
+data = data[-1,]
+rownames(data) <- data$X
+data=data[,-1]
 
-#Making Volcano Plot
+original_gene_list <- rownames(data)
+gene_list<-na.omit(original_gene_list)
+gene_list = sort(gene_list, decreasing = TRUE)
+ID<-mapIds(org.Hs.eg.db, keys = gene_list,
+           column = "ENTREZID", keytype = "SYMBOL")
 
-#ClusterProfiler
+rownames(data)<-ID
+data$ENTREZID<-ID
+data_by_celltype<-group_split(data,data$celltype)
+cell_names<-NA
 
-#Prepare Background Annotation
+for (cell in data_by_celltype){
+    cell_names<-c(cell_names,cell$celltype[1])  
+}
+cell_names<-cell_names[-1]
+names(data_by_celltype)<-cell_names
 
+nested_list<-vector("list", 21)  
+for (i in 1:21) {
+  # Create an inner list for multiples of i
+  inner_list <- data_by_celltype[[i]][[9]]
+  # Assign the inner list to the outer list
+  nested_list[[i]]<-inner_list
+}
+
+# Name the outer list elements
+names(nested_list) <- cell_names
+
+ck <- compareCluster(geneCluster = nested_list, fun = enrichKEGG)
+ck <- setReadable(ck, OrgDb = org.Hs.eg.db, keyType="ENTREZID")
+head(ck)
+
+ck_B_cell <- compareCluster(geneCluster = nested_list[1:3], fun = enrichKEGG)
+ck_B_cell <- setReadable(ck_B_cell, OrgDb = org.Hs.eg.db, keyType="ENTREZID")
+head(ck_B_cell) 
+
+ck_CD4_T_cell <- compareCluster(geneCluster = nested_list[6:9], fun = enrichKEGG)
+ck_CD4_T_cell <- setReadable(ck_CD4_T_cell, OrgDb = org.Hs.eg.db, keyType="ENTREZID")
+
+ck_CD8_T_cell <- compareCluster(geneCluster = nested_list[10:13], fun = enrichKEGG)
+ck_CD8_T_cell <- setReadable(ck_CD8_T_cell, OrgDb = org.Hs.eg.db, keyType="ENTREZID")
+
+dotplot(ck)
 
 
